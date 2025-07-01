5a8535f15b0d2a2489ffe9f7c8a6dc50d6d5d23b
YisusC <donadomolinares@gmail.com>
2025-07-01 11:53:49 -0500

    commit

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,175 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
- 
+      h1 Establecimiento del cultivo
+    .bg_img_1.mb-3
+      .bloque-texto-g.bloque-texto-g.color-terciario.p-3.p-sm-4.p-md-5.mb-5
+        .bloque-texto-g__img(
+          :style="{'background-image': `url(${require('@/assets/curso/temas/tema_1/img_1.png')})`}"
+        )
+        .bloque-texto-g__texto.p-4
+          p.mb-0 Para iniciar el cultivo de aguacate es necesario preparar bien el terreno, ya que de esto depende el buen desarrollo de las plantas.
+          p.mb-0 Lo primero es hacer una revisión del lote para conocer si el suelo, el clima, la pendiente y la calidad del agua son adecuados para sembrar.
+          p.mb-0 A continuación, se abordan los criterios técnicos para la selección del lote y los pasos necesarios para su adecuada preparación, así como las consideraciones prácticas para realizar una siembra eficiente, organizada y productiva.
+
+      separador
+
+      #t_1_1.titulo-segundo.color-acento-contenido
+        .h2(data-aos="fade-right") 1.1	Selección y preparación del lote
+      .mb-3
+
+      .row.justify-content-center.align-items-center.mb-3
+        .col-12.col-lg-2.mb-lg-5.mb-3
+          figure
+            img(src='@/assets/curso/temas/tema_1/img_2.svg', class="img-fluid")
+        .col-12.col-lg-10.mb-5
+          p.mb-0 Para establecer una plantación de aguacate es necesario identificar y acondicionar un lote que cumpla con condiciones agroecológicas favorables. 
+          p.mb-0 Este proceso requiere evaluar tanto las características físicas del terreno como la disponibilidad de servicios esenciales, incluyendo el acceso a fuentes de agua, vías de transporte y barreras naturales contra el viento.
+        .col-12.col-lg-10.mb-5
+          .tarjeta.tarjeta--fondo__slider.p-4
+            SlyderA(tipo="b").tarjeta.tarjeta--blanca.p-4
+              .row.justify-content-center.align-items-center
+                .col-12.col-lg-6.order-2.order-lg-1.mb-0
+                  p.mb-0 El aguacate prospera en suelos profundos, bien drenados, con pH entre 5,5 y 7,0, y con textura franca a franco-arenosa.
+                  p.mb-0 La profundidad efectiva del suelo debe superar los 60 cm para permitir un adecuado desarrollo radicular.
+                  p.mb-0 Es crucial evitar zonas con napas freáticas superficiales o suelos compactados que puedan generar encharcamientos y favorecer la aparición de enfermedades radiculares como <I>Phytophthora cinnamomi.</I>
+                .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+                  figure
+                    img(src='@/assets/curso/temas/tema_1/img_3.png', class="img-fluid")
+              .row.justify-content-center.align-items-center
+                .col-12.col-lg-6.order-2.order-lg-1.mb-0
+                  p.mb-0 La pendiente del terreno ideal no debe superar el 30 %. En laderas, se debe aplicar manejo de conservación de suelos, como la construcción de terrazas, zanjas de infiltración o barreras vivas siguiendo las curvas a nivel.
+                  p.mb-0 En zonas planas, el lote debe ser nivelado para evitar acumulaciones de agua y facilitar las labores mecanizadas o manuales.
+                .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+                  figure
+                    img(src='@/assets/curso/temas/tema_1/img_4.png', class="img-fluid")
+
+        .col-12.col-lg-10.mb-5
+          .row.justify-content-center.align-items-center.mb-0
+            .col-12.col-lg-9.order-2.order-lg-1
+              p.mb-0 Antes de establecer un cultivo de aguacate, es indispensable conocer las condiciones químicas y físicas del suelo mediante un análisis técnico detallado.
+              p.mb-0 Esta evaluación permite determinar los niveles de pH, salinidad, contenido de materia orgánica, textura, capacidad de intercambio catiónico (CIC), conductividad eléctrica, y concentración de nutrientes esenciales.
+              p.mb-0 Con base en estos resultados, se formulan recomendaciones específicas sobre el tipo de enmiendas requeridas para acondicionar el suelo y asegurar un entorno favorable para el desarrollo de las raíces y el establecimiento saludable de la plantación.
+            .col-12.col-lg-3.order-1.order-lg-2.mb-lg-0.mb-3
+              figure
+                img(src='@/assets/curso/temas/tema_1/img_5.png', class="img-fluid")
+
+        .col-12.col-lg-10.mb-0
+          .row.justify-content-center.align-items-center.mb-5
+            .col-8.col-lg-4.mb-lg-0.mb-3
+              figure
+                img(src="@/assets/curso/temas/tema_1/img_6.png", class="img-fluid")
+            .col-12.col-lg-8
+              AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--color-contenido-terciario-10")
+                div(titulo="Cal agrícola o dolomita")
+                  p.ps-5.mb-0 Se utiliza para corregir suelos ácidos (pH < 5.5), neutralizando la toxicidad del aluminio y aumentando la disponibilidad de calcio y magnesio.
+                  p.ps-5.mb-0 La cal agrícola contiene carbonato de calcio, mientras que la dolomita también aporta magnesio.
+                  p.ps-5.mb-0 Se aplica de forma uniforme 30 a 45 días antes de la siembra, según análisis de suelo.
+                div(titulo="Yeso agrícola")
+                  p.ps-5.mb-0 Es ideal para suelos con alto contenido de sodio, ya que mejora la estructura y la permeabilidad sin modificar el pH. 
+                  p.ps-5.mb-0 Libera calcio que desplaza el sodio del complejo de intercambio, facilitando su lavado con riego o lluvia.
+                  p.ps-5.mb-0 Su uso está indicado especialmente en suelos compactados o con encostramiento superficial.
+                div(titulo="Materia orgánica")
+                  p.ps-5.mb-0 Aporta nutrientes, mejora la estructura y retención de agua, y activa la vida microbiana del suelo.
+                  p.ps-5.mb-0 Se recomienda compost bien descompuesto, estiércol tratado o bokashi, aplicados antes de la siembra o alrededor del árbol en producción.
+                  p.ps-5.mb-0 Su uso continuo contribuye a la sostenibilidad del cultivo y la salud del suelo.
+      .row.justify-content-center.align-items-center.mb-5
+        .col-12.col-lg-2.mb-lg-0.mb-3
+          figure
+            img(src='@/assets/curso/temas/tema_1/img_7.svg', class="img-fluid")
+        .col-12.col-lg-10
+          p.mb-0 Antes de ejecutar la siembra del aguacate, es fundamental llevar a cabo una secuencia de acciones técnicas que garanticen el acondicionamiento adecuado del lote.
+          p.mb-0 Estas acciones permiten optimizar las condiciones físicas, químicas e hídricas del suelo, y establecer de manera precisa la ubicación de cada planta dentro del sistema productivo.
+          p.mb-0 Para ello, se debe considerar la preparación mecánica del terreno, la disponibilidad y calidad del agua para riego, así como el trazado estratégico del lote de acuerdo con la topografía y el recorrido solar.
+          p.mb-0 A continuación, se exploran tres aspectos clave que conforman esta etapa fundamental:
+
+      .row.justify-content-center.align-items-center.mb-3
+        .col-12.col-lg-10.mb-5
+          //// puntos de calor 
+        
+          separador
+
+          #t_1_2.titulo-segundo.color-acento-contenido
+            .h2(data-aos="fade-right") 1.2	Siembra y distancias 
+          .mb-3
+
+          p.mb-0 La siembra es una actividad técnica que requiere cuidado, precisión y conocimiento del comportamiento de las plántulas en campo.
+          p.mb-5 El éxito de esta labor determina en gran medida la uniformidad de la plantación, el crecimiento inicial y la futura arquitectura del árbol.
+          
+          .row.justify-content-center.align-items-center.mb-3
+            .col-12.col-lg-8.order-2.order-lg-1
+              AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--color-contenido-terciario-10")
+                div(titulo="Época  de siembra")
+                  p.ps-5.mb-0 La siembra debe realizarse al inicio de la temporada de lluvias o con un sistema de riego instalado. 
+                  p.ps-5.mb-0 Esto garantiza que las plántulas reciban la humedad necesaria para su adaptación y minimiza el estrés hídrico.
+                  p.ps-5.mb-0 En zonas con climas bimodales, se recomienda planificar la siembra a comienzos del primer semestre.
+                div(titulo="Tipo de material vegetal")
+                  p.ps-5.mb-0 Se utilizan plántulas injertadas entre 60 y 120 cm de altura, provenientes de viveros certificados.
+                  p.ps-5.mb-0 El injerto debe estar bien soldado, con brotes vigorosos, sistema radicular sano y libre de plagas o enfermedades.
+                  p.ps-5.mb-0 La bolsa plástica debe retirarse con cuidado para no dañar las raíces; se recomienda hacer un corte inferior y deslizar hacia arriba.
+                div(titulo="Ahoyado y trasplante")
+                  p.ps-5.mb-0 Los hoyos deben abrirse con al menos 30 días de anticipación y tener dimensiones mínimas de 40 cm de ancho por 50 cm de profundidad.
+                  p.ps-5.mb-0 Se deben llenar con una mezcla de tierra fértil, materia orgánica y enmiendas como fosfato diamónico o cal dolomita, según el análisis de suelo.
+                  p.ps-5.mb-0 El árbol se planta de forma que el cuello de la raíz quede ligeramente por encima del nivel del suelo. 
+                  p.ps-5.mb-0 El hueco se rellena con tierra fina y se compacta ligeramente para evitar bolsas de aire.
+                  p.ps-5.mb-0 Se debe dejar un montículo de 10 cm alrededor del tallo para evitar acumulación de agua.
+                  p.ps-5.mb-0 Se finaliza con un riego de asentamiento y, de ser posible, se coloca cobertura vegetal seca (<I>mulch</I>).
+                div(titulo="Distancias de siembra")
+                  p.ps-5.mb-0 El marco de siembra más utilizado es de 7 X 7 metros, equivalente a 204 árboles por hectárea.
+                  p.ps-5.mb-0 Esta densidad permite un adecuado equilibrio entre penetración de luz, desarrollo radicular y manejo sanitario.
+                  p.ps-5.mb-0 En suelos fértiles o con riego, se pueden usar distancias menores (6 X 6 m), mientras que en terrenos de ladera o con baja fertilidad, se prefieren marcos de 8 X 8 o 9 X 9 m para reducir la competencia.
+
+            .col-9.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+              figure
+                img(src='@/assets/curso/temas/tema_1/img_9.png', class="img-fluid")
+
+          p.mb-0 A continuación, se presenta una tabla orientativa de densidad de siembra que permite estimar el número de árboles por hectárea según el marco de plantación seleccionado. 
+          p.mb-0 La elección de la distancia depende del vigor del patrón, la fertilidad del suelo y las condiciones topográficas del lote. 
+          p.mb-5 Esta información facilita la planificación eficiente del cultivo y el manejo agronómico futuro.
+
+          .titulo-sexto.color-acento-contenido
+            h5 Tabla 1.
+            |  Distribución orientativa de densidad de siembra.
+          .tabla-a.mb-5
+            table
+              caption 
+                p <b>Fuente.</b> SENA, 2025.
+              thead
+                tr
+                  th Distancia (m)
+                  th Tipo de patrón
+                  th Árboles/ ha (cuadrado)
+                  th Árboles/ ha (triángulo)
+              tbody
+                tr
+                  td 6 X 6
+                  td Alto vigor
+                  td 278
+                  td 321
+                tr
+                  td 7 X 7
+                  td Medio vigor
+                  td 204
+                  td 236
+                tr
+                  td 8 X 8
+                  td Alto vigor
+                  td 156
+                  td 180
+                tr
+                  td 9 X 9
+                  td Bajo vigor
+                  td 123
+                  td 142
+    .bloque-texto-t.justify-content-center.align-items-center.mb-0
+      .bloque-texto-t__img.mb-0
+        img(src='@/assets/curso/temas/tema_1/img_10.png')
+      .bloque-texto-t__texto.col-10.mb-0
+        p.mb-0 Después de la siembra, se recomienda realizar un seguimiento semanal para verificar prendimiento, detectar posibles problemas sanitarios y realizar resiembras en caso necesario. 
+        p.mb-0 Durante los primeros tres meses, el árbol debe mantenerse libre de malezas, bien tutorado y con humedad constante en el plato.
+        p.mb-0 El éxito de esta etapa garantiza un sistema radicular fuerte y una planta bien conformada para iniciar el ciclo productivo.
+
+
 </template>
 
 <script>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,86 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Tema 2
+      h1 Manejo agronómico 
+    .bg_img_2.mb-3
+      .row.justify-content-center.align-items-center.mb-0
+        .col-12.col-lg-5.mb-lg-0.mb-3
+          figure
+            img(src='@/assets/curso/temas/tema_2/img_1.png', class="img-fluid")
+        .col-12.col-lg-7.mb-0
+          p.mb-0 El manejo agronómico del cultivo de aguacate comprende todas las actividades que permiten el crecimiento saludable de las plantas desde su establecimiento hasta la fase productiva.
+          p.mb-0 Estas labores deben planificarse de manera técnica, respetando los requerimientos específicos de la especie y su adaptación al entorno. 
+          p.mb-0 En esta etapa, se destaca la importancia de las labores culturales como el tutorado, el plateo y las podas, además del diseño y aplicación de un programa de fertilización racional y un plan de Manejo Integrado de Plagas y Enfermedades (MIPE), que asegure la sanidad vegetal, la sostenibilidad del cultivo y la calidad de los frutos.
+
+      separador
+
+      #t_2_1.titulo-segundo.color-acento-contenido
+        .h2(data-aos="fade-right") 2.1 Labores culturales
+      .mb-3
+
+      .row.justify-content-center.align-items-center.mb-0
+        .col-12.col-lg-2.mb-lg-5.mb-3
+          figure
+            img(src='@/assets/curso/temas/tema_2/img_2.png', class="img-fluid")
+        .col-12.col-lg-10.mb-5
+          p.mb-0 Durante los primeros meses del establecimiento del cultivo, las plantas son vulnerables a factores climáticos como el viento, así como a interferencias con otras especies vegetales (arvenses) que compiten por agua, luz y nutrientes.
+          p.mb-0 Por eso, es fundamental realizar labores culturales oportunas que contribuyan a la formación estructural de los árboles, al mejoramiento del suelo y a la prevención de problemas sanitarios.
+        
+        .col-12.col-lg-10.mb-0
+          .tarjeta.tarjeta--fondo__slider.p-4
+            SlyderA(tipo="b").tarjeta.tarjeta--blanca.p-4
+              .row.justify-content-center.align-items-center
+                .col-12.col-lg-6.order-2.order-lg-1.mb-0
+                  h5 Tutorado
+                  p.mb-0 Se recomienda tutorar cada plántula al momento de la siembra, especialmente en zonas con vientos fuertes o lluvias intensas.
+                  p.mb-0 El tutor se coloca de forma vertical, a 10 cm de la base del árbol, sin perforar la bolsa radicular ni dañar las raíces.
+                  p.mb-0 Se utiliza cuerda de yute o cinta biodegradable para sujetar el tallo al tutor con amarre en forma de “8” que evite la estrangulación del tallo. 
+                  p.mb-0 Esta labor asegura que el tallo crezca recto, facilitando futuras podas de formación y reduciendo el riesgo de quiebre o inclinación.
+                .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+                  figure
+                    img(src='@/assets/curso/temas/tema_2/img_3.png', class="img-fluid")
+              .row.justify-content-center.align-items-center
+                .col-12.col-lg-6.order-2.order-lg-1.mb-0
+                  h5 Plateo
+                  p.mb-0 Consiste en limpiar una zona circular alrededor del tronco, llamada “plato”, para mejorar la absorción de agua y nutrientes. El diámetro del plateo debe corresponder al tamaño de la copa. 
+                  p.mb-0 Esta técnica también permite aplicar fertilizantes o materia orgánica de forma localizada y evita la competencia con malezas.
+                  p.mb-0 Para mantener la humedad y proteger el sistema radicular, se recomienda cubrir el plato con <I>mulch</I> orgánico como hojas secas, paja o cáscara de arroz.
+                .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+                  figure
+                    img(src='@/assets/curso/temas/tema_2/img_4.png', class="img-fluid")
+              .row.justify-content-center.align-items-center
+                .col-12.col-lg-6.order-2.order-lg-1.mb-0
+                  h5 Podas
+                  p.mb-0 Las podas son fundamentales para dirigir el crecimiento del árbol, evitar entrecruzamientos y mejorar la entrada de luz y aire. 
+                  p.mb-0 Existen dos tipos principales:
+                  ul.mb-0
+                    li <b>Poda de formación:</b> se realiza durante los dos primeros años para establecer una estructura de copa equilibrada.
+                        p.mb-0 Se seleccionan entre 3 a 5 ramas principales distribuidas en diferentes direcciones, con ángulos amplios respecto al tallo.
+                        p.mb-0 Se eliminan ramas con orientación hacia abajo, cruzadas o débiles.
+                    li <b>Poda de mantenimiento:</b> se realiza a partir del tercer año para remover ramas secas, enfermas, mal orientadas o brotes innecesarios.
+                        p.mb-0 Esta poda reduce la incidencia de plagas y enfermedades, mejora la calidad del fruto y prolonga la vida útil del árbol.
+                  p.mb-0 Cada corte debe hacerse con herramientas desinfectadas (alcohol al 70 % o hipoclorito de sodio al 5 %) y, preferiblemente, aplicar cicatrizante a base de cobre en los cortes gruesos.
+                .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+                  figure
+                    img(src='@/assets/curso/temas/tema_2/img_5.png', class="img-fluid")
+
+      separador
+
+      #t_2_2.titulo-segundo.color-acento-contenido
+        .h2(data-aos="fade-right") 2.2 Fertilización y MIPE
+      .mb-3
+
+      .row.justify-content-center.align-items-center.mb-0
+        .col-12.col-lg-4.mb-lg-5.mb-3
+          figure
+            img(src='@/assets/curso/temas/tema_2/img_7.png', class="img-fluid")
+        .col-12.col-lg-8.mb-5
+          p.mb-0 La correcta nutrición del cultivo de aguacate y el manejo fitosanitario sostenible son pilares fundamentales para lograr productividad, calidad e inocuidad del fruto.
+          p.mb-0 Para ello, se deben implementar de manera articulada prácticas de fertilización balanceada y el enfoque de Manejo Integrado de Plagas y Enfermedades (MIPE), el cual permite reducir el uso indiscriminado de agroquímicos, preservar la biodiversidad y cumplir con estándares de sostenibilidad exigidos por los mercados actuales.
+        
+        .col-12.col-lg-10.mb-5
+
+
 
 </template>
 
